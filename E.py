4b92f335d9c2bc1ba6632f1e38e6e4d3d31a4871
Sofia Avallone Sakovitz <sas3@WINDOWS.CIN.UFPE.BR>
2025-10-03 16:46:50 -0300

    segunda e terceira questões

---
+++ E.py
@@ -0,0 +1,8 @@
+bolinhas_andre = int("Digite a quantidade de bolinhas iniciais de André: ")
+bolinhas_bruno = int("Digite a quantidade de bolinhas iniciais de Bruno: ")
+bolinhas_clara = int("Digite a quantidade de bolinhas iniciais de Clara: ")
+
+cont_erros_andre = 0
+cont_erros_bruno = 0
+cont_erros_clara = 0
+    
