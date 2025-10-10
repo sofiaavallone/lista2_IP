14c83ff45891f5e124cebc1b4a6eb97f5cb52261
Sofia Avallone Sakovitz <sas3@WINDOWS.CIN.UFPE.BR>
2025-10-10 17:01:48 -0300

    finalizando questão 5 e fazendo 8

--- G.py
+++ G.py
@@ -1,86 +1,89 @@
 print("Começa agora o treinamento para grande final mundial de cabo de guerra!")
 quant_partidas = 0
+# Loop com condição para o número de partidas serem ímpar
 while quant_partidas%2 == 0:
     if quant_partidas != 0:
         print("Não deverá haver empates! O número de partidas deverá ser ímpar.")
     quant_partidas = int(input())
 print(f"Eles batalharão em {quant_partidas} longas partidas.")
 
+# Entradas
 forca_arthur = int(input())
 forca_joao = int(input())
 resistencia_inicial = int(input())
 
-resistencia_arthur = resistencia_inicial
-resistencia_joao = resistencia_inicial
-pontos_rodada_joao = 0
-pontos_rodada_arthur = 0
+# Variáveis para saber quantas partidas ganhou
 pontos_partida_joao = 0
 pontos_partida_arthur = 0
 
 i=0
-vencedor_pontos = 0
-while (i <= quant_partidas) and (quant_partidas-vencedor_pontos > quant_partidas-i):
+while (i < quant_partidas) and (abs(pontos_partida_arthur-pontos_partida_joao) <= quant_partidas-i): # Partidas
     i+=1
     print(f"Começa a {i}ª partida!")
     print(f"Placar geral: {pontos_partida_arthur} X {pontos_partida_joao}")
+    resistencia_arthur = resistencia_inicial
+    resistencia_joao = resistencia_inicial
 
-    while resistencia_arthur != 0 and resistencia_joao != 0:
-        primo = False
+    # Variáveis para saber quantas rodadas ganhou
+    pontos_rodada_joao = 0
+    pontos_rodada_arthur = 0
+
+    while resistencia_arthur != 0 and resistencia_joao != 0: # Rodadas
+        primo = True
         num = int(input())
-        for i in range(num-1, 2):
-            if num % i == 0:
-                primo = True
+        if num < 2:
+            primo = False
+        else:
+            for j in range(2, num): # Conferindo se o número é primo
+                if num % j == 0:
+                    primo = False
         
-        if primo == True: # João
+        if primo == True: # João ganhou a rodada
             resistencia_joao+=1
-            resistencia_joao-=1
-            pontos_rodada_joao+=1
-            print("O número é um quadrado perfeito! Arthur consegue puxar mais forte.")
-        elif type(num**0.5) == int: # Arthur
-            resistencia_arthur+=1
             resistencia_arthur-=1
+            pontos_rodada_joao+=1
+            print("O primo do primo é primo do primo? João puxa mais forte!")
+        elif int(num**0.5) == num**0.5: # Arthur ganhou a rodada
+            resistencia_arthur+=1
+            resistencia_joao-=1
             pontos_rodada_arthur+=1
             print("O número é um quadrado perfeito! Arthur consegue puxar mais forte.")
         else:
-            if forca_arthur>forca_joao:
+            if forca_arthur>forca_joao: # Arthur ganhou por força
                 resistencia_arthur+=1
                 resistencia_joao-=1
                 pontos_rodada_arthur+=1
                 print("Arthur é o mais forte! João não consegue segurar.")
-            else:
+            else: # João ganhou por força
                 resistencia_joao+=1
                 resistencia_arthur-=1
                 pontos_rodada_joao+=1
                 print("João é o mais forte! Arthur não consegue segurar.")
     
-    if pontos_rodada_joao>pontos_partida_arthur:
-        pontos_rodada_joao+=1
+    if pontos_rodada_joao>pontos_rodada_arthur: # João ganhou a partida
+        pontos_partida_joao+=1
         print("João usa seus talentos de mangueboy e leva essa para casa!")
-    else:
+    else: # Arthur ganhou a partida
         pontos_partida_arthur+=1
         print("Arthur dá orgulho para Maceió e ganha a partida!")
-    
-    if pontos_partida_arthur>pontos_partida_joao:
-        vencedor = pontos_partida_arthur
-    else:
-        vencedor_pontos = pontos_partida_joao
 
 print()
 print("Agora eles estão prontos para o mundial!")
-print(f"Placar final: {resistencia_arthur} X {resistencia_joao}")
-if vencedor_pontos == pontos_partida_joao:
+print(f"Placar final: {pontos_partida_arthur} X {pontos_partida_joao}")
+# Conferindo quem ganhou/perdeu
+if pontos_partida_joao>pontos_partida_arthur:
     perdedor = "Arthur"
     vencedor = "João"
-elif vencedor_pontos == pontos_partida_arthur:
+else:
     perdedor = "João"
     vencedor = "Arthur"
 
+# Prints específicos dos resultados
 if perdedor == "Arthur" and pontos_partida_arthur == 0:
     print(f"{perdedor} não teve chance! {vencedor} venceu todas as partidas.")
 elif perdedor == "João" and pontos_partida_joao == 0:
     print(f"{perdedor} não teve chance! {vencedor} venceu todas as partidas.")
 elif vencedor == "Arthur":
-    print(f"O ganhador foi {vencedor} com uma diferença de {pontos_partida_arthur} partidas.")
+    print(f"O ganhador foi {vencedor} com uma diferença de {pontos_partida_arthur-pontos_partida_joao} partidas.")
 else:
-    print(f"O ganhador foi {vencedor} com uma diferença de {pontos_partida_joao} partidas.")
-
+    print(f"O ganhador foi {vencedor} com uma diferença de {pontos_partida_joao-pontos_partida_arthur} partidas.")
---
+++ H.py
@@ -0,0 +1,143 @@
+vivos1 = 6
+vivos2 = 6
+mortos1 = 0
+mortos2 = 0
+print("Serão 12 desenvolvedores defendendo a honra de seus lados do código! Que vença a melhor stack!")
+
+# Definindo os times
+time1 = input()
+while time1 != "Front-End" and time1 != "Back-End":
+    print("Entrada inválida!")
+    time1 = input()
+
+if time1 == "Front-End":
+    time2 = "Back-End"
+else:
+    time2 = "Front-End"
+
+time_jogando = time1
+lugar = "c"
+while vivos1 != 0 and vivos2 !=0:
+    
+
+    resultado_ataque = input()
+    while resultado_ataque != "acertou" and resultado_ataque != "errou":
+        print("Entrada inválida!")
+        resultado_ataque = input()
+    else:
+        # Ataque em campo
+        while lugar == "c":
+            # Time 1 atacando
+            if time_jogando == time1:
+                if resultado_ataque == "acertou":
+                    print(f"{time1} acertou um jogador!")
+                    vivos2-=1
+                    mortos2+=1
+                    if time1 == "Back-End":
+                        print(f"Back-End: {vivos1} dev(s) em campo. | Front-End: {vivos2} dev(s) em campo.")
+                    else:
+                        print(f"Back-End: {vivos2} dev(s) em campo. | Front-End: {vivos1} dev(s) em campo.")
+                    lugar = "m"
+                    time_jogando = time2
+                else:
+                    if mortos1 == 0:
+                        time_jogando = time2
+                    else:
+                        resultado_defesa = input()
+                        while resultado_defesa != "pegou" and resultado_defesa != "deixou":
+                            print("Entrada inválida!")
+                            resultado_defesa = input()
+                        if resultado_defesa == "pegou":
+                            time_jogando = time2
+                        else:
+                            lugar = "m"
+            # Time 2 atacando
+            if time_jogando == time2 and lugar == "c":
+                if resultado_ataque == "acertou":
+                    print(f"{time2} acertou um jogador!")
+                    vivos1-=1
+                    mortos1+=1
+                    if time1 == "Back-End":
+                        print(f"Back-End: {vivos1} dev(s) em campo. | Front-End: {vivos2} dev(s) em campo.")
+                    else:
+                        print(f"Back-End: {vivos2} dev(s) em campo. | Front-End: {vivos1} dev(s) em campo.")
+                    lugar = "m"
+                    time_jogando = time1
+                else:
+                    if mortos2 == 0:
+                        time_jogando = time1
+                    else:
+                        resultado_defesa = input()
+                        while resultado_defesa != "pegou" and resultado_defesa != "deixou":
+                            print("Entrada inválida!")
+                            resultado_defesa = input()
+                        if resultado_defesa == "pegou":
+                            time_jogando = time1
+                        else:
+                            lugar = "m"
+        # Ataque no morto
+        while lugar == "m":
+            # Morto time 1
+            if time_jogando == time1:  
+                resultado_ataque = input()
+                while resultado_ataque != "acertou" and resultado_ataque != "errou":
+                    print("Entrada inválida!")
+                    resultado_ataque = input()
+                if resultado_ataque == "acertou":
+                    print(f"O morto do {time1} acertou um jogador!")
+                    vivos1+=1
+                    mortos1-=1
+                    vivos2-=1
+                    mortos2+=1
+                    if time1 == "Back-End":
+                        print(f"Back-End: {vivos1} dev(s) em campo. | Front-End: {vivos2} dev(s) em campo.")
+                    else:
+                        print(f"Back-End: {vivos2} dev(s) em campo. | Front-End: {vivos1} dev(s) em campo.")
+                    time_jogando = time2
+                else:
+                    resultado_defesa = input()
+                    while resultado_defesa != "pegou" and resultado_defesa != "deixou":
+                        print("Entrada inválida!")
+                        resultado_defesa = input()
+                    if resultado_defesa == "pegou":
+                        time_jogando = time2
+                        lugar = "c"
+                    else:
+                        lugar = "c"
+                        time_jogando = time2
+            # Morto time 2
+            else:
+                resultado_ataque = input()
+                while resultado_ataque != "acertou" and resultado_ataque != "errou":
+                    print("Entrada inválida!")
+                    resultado_ataque = input()
+                if resultado_ataque == "acertou":
+                    print(f"O morto do {time2} acertou um jogador!")
+                    vivos2+=1
+                    mortos2-=1
+                    vivos1-=1
+                    mortos1+=1
+                    if time1 == "Back-End":
+                        print(f"Back-End: {vivos1} dev(s) em campo. | Front-End: {vivos2} dev(s) em campo.")
+                    else:
+                        print(f"Back-End: {vivos2} dev(s) em campo. | Front-End: {vivos1} dev(s) em campo.")
+                    time_jogando = time1
+                else:
+                    resultado_defesa = input()
+                    while resultado_defesa != "pegou" and resultado_defesa != "deixou":
+                        print("Entrada inválida!")
+                        resultado_defesa = input()
+                    if resultado_defesa == "pegou":
+                        time_jogando = time1
+                        lugar = "c"
+                    else:
+                        lugar = "c"
+                        time_jogando = time1
+if vivos1>vivos2 and time1 == "Back-End":
+    print(f"Vitória do Back-End! Restaram {vivos1} devs ainda mantendo o servidor de pé!")
+elif vivos1>vivos2 and time2 == "Back-End":
+    print(f"Vitória do Back-End! Restaram {vivos2} devs ainda mantendo o servidor de pé!")
+elif vivos2>vivos1 and time1 == "Front-End":
+    print(f"Vitória do Front-End! Restaram {vivos1} devs ainda segurando o layout!")
+elif vivos2>vivos1 and time1 == "Front-End":
+    print(f"Vitória do Front-End! Restaram {vivos2} devs ainda segurando o layout!")
